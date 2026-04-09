c8b41cce858f1290735b786900b51a3317e056be
Davi Mostaro <davimostaroprofissional@gmail.com>
2026-04-08 22:12:14 -0300

    feat: adiciona iniciaLista, limpaLista e adicionaElemento

--- src/lista.js
+++ src/lista.js
@@ -1,6 +1,18 @@
-const lista = ["um", "dois", "três"];
+let lista = [];
+
+export function iniciaLista(){
+    lista = ["um", "dois", "três"];
+}
 
 export function getLista() {
     const listaCopia = structuredClone(lista);
     return listaCopia;
+}
+
+export function limpaLista() {
+    lista.splice(0);
+}
+
+export function adicionaElemento(elemento) {
+    lista.push(elemento);
 }
--- src/lista.test.js
+++ src/lista.test.js
@@ -1,13 +1,15 @@
 import { expect, it } from "vitest";
-import { getLista } from "./lista";
+import { adicionaElemento, getLista, iniciaLista, limpaLista } from "./lista";
 
 it("deve ter elementos iniciais", () => {
+    iniciaLista();
     const lista = getLista();
 
     expect(lista).toHaveLength(3);   
 });
 
 it("alterar a lista retornada não deve afetar a original", () => {
+    iniciaLista();
     const lista = getLista();
     expect(lista).toHaveLength(3);
 
@@ -15,3 +17,25 @@ it("alterar a lista retornada não deve afetar a original", () => {
     const lista2 = getLista();
     expect(lista2).toHaveLength(3);
 });
+
+it("lista deve ter 0 elementos após limpeza", () => {
+    iniciaLista();
+    const lista = getLista();
+    expect(lista).toHaveLength(3);
+
+    limpaLista();
+    const lista2 = getLista();
+    expect(lista2).toHaveLength(0);
+});
+
+
+it("deve adicionar um elemento à lista", () => {
+    iniciaLista();
+    const lista = getLista();
+    expect(lista).toHaveLength(3);
+
+    adicionaElemento("quatro");
+    const lista2 = getLista();
+    expect(lista2).toHaveLength(4);
+});
+
